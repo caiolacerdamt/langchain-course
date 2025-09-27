4f7de6700a06dcf127069adb33aa23a6de168c05
caiolacerdamt <caiolacerdamt@gmail.com>
2025-09-27 12:23:57 -0300

    Add initial project files: .env.example, .gitignore, requirements.txt, and teste.py

---
+++ teste.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+# Carregando as variaveis de '.env'
+load_dotenv()
+
+# Chama a API do modelos da Open IA.
+model = ChatOpenAI(model="gpt-4o")
+
+# Executa a chamada ao modelo
+result = model.invoke("Este é um teste. Se você recebeu a requisição responda 'Teste OK'.")
+print(result)
