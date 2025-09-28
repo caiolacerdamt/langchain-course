54c1a4bebad2c5b2c74430da7800b72efbea1111
caiolacerdamt <caiolacerdamt@gmail.com>
2025-09-28 18:11:05 -0300

    Running the test script

--- teste.py
+++ teste.py
@@ -4,8 +4,8 @@ from langchain_openai import ChatOpenAI
 # Carregando as variaveis de '.env'
 load_dotenv()
 
-# Chama a API do modelos da Open IA.
-model = ChatOpenAI(model="gpt-4o")
+# Chama a API do modelos da Open IA
+model = ChatOpenAI(model="gpt-5-mini")
 
 # Executa a chamada ao modelo
 result = model.invoke("Este é um teste. Se você recebeu a requisição responda 'Teste OK'.")
