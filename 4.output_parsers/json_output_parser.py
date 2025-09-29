0c253ef1a50e76241f82ac6ee158dd636fd79dab
caiolacerdamt <caiolacerdamt@gmail.com>
2025-09-29 18:06:54 -0300

    Add JSON output parser with langchain

---
+++ 4.output_parsers/json_output_parser.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.prompts import ChatPromptTemplate
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
+from langchain_openai.chat_models import ChatOpenAI
+from pydantic import BaseModel, Field
+
+model = ChatOpenAI(model="gpt-5-mini")
+
+class Rota(BaseModel):
+    escolha: int = Field(description="Rota escolhida")
+    pensamento: str = Field(description="Campo para o pensamento que levou a decisão da rota escolhida")
+    resposta: str = Field(description="Resposta a pergunta do usuário")
+
+parser = JsonOutputParser(pydantic_object=Rota)
+
+prompt_template = ChatPromptTemplate([("system",
+                                       "Se a pergunta do usuário for relacionado ao setor financeiro, \
+                                       a escolha deve ser 1, caso contrário a escolha pode ser qualquer numero \
+                                       diferente de 1. \n{format_instructions}\n Pergunta Usuário: {pergunta_user}")],
+                                     partial_variables={"format_instructions": parser.get_format_instructions()})
+
+chain = prompt_template | model | parser
+
+output = chain.invoke({"pergunta_user": "Me diga quanto está o dolar"})
+
+print(output)
+print("----"*20)
+print(output['escolha'])
+print("----"*20)
+print(output['pensamento'])
+print("----"*20)
+print(output['resposta'])
